d20ae018fd65c2ab02369ea1fbd04a80dc44eff6
GIIIPFEL <kindlerherbert@gmail.com>
2022-04-05 18:20:05 +0200

    Update index.php

--- kunden/index.php
+++ kunden/index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["username"])) {
   <link rel="stylesheet" href="master.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tirarzt Kindler | Webinterface</title>
+  <title>Tirarzt | Webinterface</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
